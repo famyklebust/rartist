6b8665b3f3bbfbbab531c9ea379b0396e6623de9
Frode Myklebust <myklebust89@gmail.com>
2017-03-26 22:11:33 +0100

    Form handling + improved conditional logic

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var app = express();
 
 // depencencies
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
 // controllers
 var lastfmEngine = require('./controllers/lastfm');
@@ -15,25 +16,48 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(express.static('public'));
 app.use(cookieParser());
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 // declare variables
-var lastfmUser, topArtistsList;
+var lastfmUser, topArtists;
 
 app.get('/', function (req, res) {
+	topArtists = 0;
 	if (!req.cookies.lastfmUser) {
 		lastfmUser = 0;
-		topArtistsList = 0;
 	} else {
 		lastfmUser = req.cookies.lastfmUser;
-		lastfmEngine.getTopArtists(function (topArtists) {
-			topArtistsList = topArtists;
-		});
 	}
 	res.render('index', {
 		title: 'rartist',
 		lastfmUser: lastfmUser,
-		topArtists: topArtistsList
+		topArtists: topArtists
 	});
+});
+
+app.post('/', urlencodedParser, function (req, res) {
+	if (!req.body) {
+		res.redirect('/');
+	} else {
+		res.cookie('lastfmUser', req.body.username);
+		res.redirect('/');
+	}
+});
+
+app.get('/recommend', function (req, res) {
+	if (!req.cookies.lastfmUser) {
+		lastfmUser = 0;
+		topArtists = 0;
+	} else {
+		lastfmUser = req.cookies.lastfmUser;
+		lastfmEngine.getTopArtists(function (topArtists) {
+			res.render('index', {
+				title: 'rartist',
+				lastfmUser: lastfmUser,
+				topArtists: topArtists
+			});
+		});
+	}
 });
 
 app.listen(3000, function () {
